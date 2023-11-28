e60e230e3c42c092b8afcd62991606287db96c4b
OlekZima <zimenkoalex34@gmail.com>
2023-11-28 21:50:00 +0100

    Day_8 cipher fixed. Modulo is added and a main loop

--- Day_8/main_day_task.py
+++ Day_8/main_day_task.py
@@ -4,14 +4,14 @@ from caeser_cipher import logo
 def caesar_cipher(text: str, shift: int, encrypt=True):
     text = text.lower()
     encrypted_text = ""
-    for letter in text:
-        if not letter.isalpha():
-            encrypted_text += letter
+    for char in text:
+        if not char.isalpha():
+            encrypted_text += char
             continue
-        index = ascii_lowercase.index(letter)
+        index = ascii_lowercase.index(char)
         new_letter_index = (index + shift) if encrypt else (index - shift)
         if new_letter_index >= len(ascii_lowercase):
-            new_letter_index -= len(ascii_lowercase)
+            new_letter_index %= len(ascii_lowercase)
         encrypted_text += ascii_lowercase[new_letter_index]
     else:
         return encrypted_text
@@ -20,11 +20,18 @@ def caesar_cipher(text: str, shift: int, encrypt=True):
 
 if __name__ == "__main__":
     print(logo)
-    encode = input("Encode or decore?\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
 
-    if encode.lower() == "encode":
-        print(f"Your encoded message is: {caesar_cipher(text, shift)}")
-    elif encode.lower() == "decode":
-        print(f"Your decoded message is: {caesar_cipher(text, shift, encrypt=False)}")
+    is_continue = True
+    while is_continue:
+            
+        encode = input("Encode or decore?\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+
+        if encode.lower() == "encode":
+            print(f"Your encoded message is: {caesar_cipher(text, shift)}")
+        elif encode.lower() == "decode":
+            print(f"Your decoded message is: {caesar_cipher(text, shift, encrypt=False)}")
+
+        user_continue = input("Do you want to restart the program? yesr or no\n").lower()
+        is_continue = True if user_continue == "yes" else False
