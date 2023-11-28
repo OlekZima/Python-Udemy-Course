eb797c5af41a482a026f22bed40d8abb84930c22
OlekZima <zimenkoalex34@gmail.com>
2023-11-28 21:43:53 +0100

    Day 8 is complete. Art is also added

--- Day_8/main_day_task.py
+++ Day_8/main_day_task.py
@@ -1,9 +1,13 @@
 from string import ascii_lowercase
+from caeser_cipher import logo
 
 def caesar_cipher(text: str, shift: int, encrypt=True):
     text = text.lower()
     encrypted_text = ""
     for letter in text:
+        if not letter.isalpha():
+            encrypted_text += letter
+            continue
         index = ascii_lowercase.index(letter)
         new_letter_index = (index + shift) if encrypt else (index - shift)
         if new_letter_index >= len(ascii_lowercase):
@@ -12,28 +16,15 @@ def caesar_cipher(text: str, shift: int, encrypt=True):
     else:
         return encrypted_text
 
-def encrypt(text: str, shift: int):
-    text = text.lower()
-    encrypted_text = ""
-    for letter in text:
-        index = ascii_lowercase.index(letter)
-        new_letter_index = index + shift
-        if new_letter_index >= len(ascii_lowercase):
-            new_letter_index = new_letter_index - len(ascii_lowercase)
-        encrypted_text += ascii_lowercase[new_letter_index]
-    else:
-        return encrypted_text
-    
-
 # print(len(ascii_lowercase))
 
 if __name__ == "__main__":
+    print(logo)
     encode = input("Encode or decore?\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
     if encode.lower() == "encode":
-        print(caesar_cipher(text, shift))
+        print(f"Your encoded message is: {caesar_cipher(text, shift)}")
     elif encode.lower() == "decode":
-        print(caesar_cipher(text, shift, encrypt=False))
-
+        print(f"Your decoded message is: {caesar_cipher(text, shift, encrypt=False)}")
