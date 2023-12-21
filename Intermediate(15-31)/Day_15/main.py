6a5defbad2f61a8c9d2bbe47fcfcad3ac7513dc1
OlekZima <zimenkoalex34@gmail.com>
2023-12-21 22:45:16 +0100

    int input validation and counting money functions

--- Intermediate(15-31)/Day_15/main.py
+++ Intermediate(15-31)/Day_15/main.py
@@ -1,6 +1,5 @@
 from typing import Dict
 
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -33,11 +32,43 @@ resources = {
     "coffee": 100,
 }
 
+
 def print_resources(resources: Dict[str, int], money: float) -> None:
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
     print(f"Money: ${money}")
 
+
+def validate_int_input(prompt: str) -> int:
+    is_validated = False
+    return_val = 0
+    while not is_validated:
+        try:
+            return_val = int(input(prompt))
+            is_validated = True
+        except ValueError:
+            print("Invalid input!")
+    return return_val
+
+
+def count_money(coffee_price: float):
+    quarters_val = validate_int_input("How many quarters?: ") * 0.25
+    dimes_val = validate_int_input("How many dimes?: ") * 0.10
+    nickles_val = validate_int_input("How many nickles?: ") * 0.05
+    pennies_val = validate_int_input("How many pennies?: ") * 0.01
+    money = quarters_val + dimes_val + nickles_val + pennies_val
+    if money < coffee_price:
+        print("Sorry that's not enough money. Money refunded.")
+        return 0
+    else:
+        change = round(money - coffee_price, 2)
+        print(f"Here is ${change} in change.")
+        return coffee_price
+
+
+def coffee_machine() -> None:
+    money = 0.0
+
 if __name__ == '__main__':
-    print_resources(resources, 2.5)
+    count_money(2.5)
