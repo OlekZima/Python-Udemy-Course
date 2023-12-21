25a0e5fe8787bb90f3b9e82ac07431ab4b7129b3
OlekZima <zimenkoalex34@gmail.com>
2023-12-21 22:30:23 +0100

    print resources function

--- Intermediate(15-31)/Day_15/main.py
+++ Intermediate(15-31)/Day_15/main.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -30,3 +33,11 @@ resources = {
     "coffee": 100,
 }
 
+def print_resources(resources: Dict[str, int], money: float) -> None:
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money}")
+
+if __name__ == '__main__':
+    print_resources(resources, 2.5)
