9c4974d03a0a5b96f07e1b76766893127bf8c695
OlekZima <zimenkoalex34@gmail.com>
2024-01-10 16:07:08 +0100

    Add Ball class and implement move_to_the_point method

--- Intermediate(15-31)/Day_21/ball.py
+++ Intermediate(15-31)/Day_21/ball.py
@@ -1,8 +1,25 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__(shape="circle")
         self.penup()
+        self.speed("fastest")
         self.color("white")
-        
+
+    def move_to_the_point(self, x: int, y: int, speed: float = 0.5):
+        # x -= 20
+        # y -= 20
+        slope = 0
+        try:
+            slope: float = (y - self.ycor()) / (x - self.xcor())
+        except ZeroDivisionError:
+            print("lol, zero division")
+
+        x_step: float = speed
+
+        print(slope)
+
+        while self.xcor() < x and self.ycor() < y:
+            self.goto(self.xcor() + x_step, self.ycor() + x_step * slope)
--- Intermediate(15-31)/Day_21/main.py
+++ Intermediate(15-31)/Day_21/main.py
@@ -1,6 +1,7 @@
 import turtle as t
 from screen_turtle import ScreenTurtle
 from paddle import Paddle
+from ball import Ball
 
 
 def init_screen(width: int, height: int) -> t._Screen:
@@ -19,6 +20,8 @@ def main(width: int, height: int):
 
     screen_turtle = ScreenTurtle(width, height)
 
+    ball = Ball()
+
     player = Paddle(width)
     screen.onkeypress(key="w", fun=player.move_up)
     screen.onkeypress(key="s", fun=player.move_down)
@@ -27,7 +30,17 @@ def main(width: int, height: int):
     screen.onkeypress(key="Up", fun=computer.move_up)
     screen.onkeypress(key="Down", fun=computer.move_down)
 
-    while True:
+    # ball_tick_speed =
+    target_x, target_y = width / 2 - 20, height / 2 - 20
+    slope = (target_y - ball.ycor()) / (target_x - ball.xcor())
+    x_step = 0.5
+
+    is_game = True
+    while is_game:
+        ball_x, ball_y = ball.pos()
+        if ball_x < target_x and ball_y < target_y:
+            ball.goto(ball.xcor() + x_step, ball.ycor() + x_step * slope)
+
         screen.update()
 
     screen.exitonclick()
