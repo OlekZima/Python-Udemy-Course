0f964dd77b89add701045c6b1d34dca9af6829dd
OlekZima <zimenkoalex34@gmail.com>
2024-01-21 00:37:26 +0100

    Add Scoreboard class with write_level method

--- Intermediate(15-31)/Day_22/scoreboard.py
+++ Intermediate(15-31)/Day_22/scoreboard.py
@@ -1,5 +1,15 @@
+from turtle import Turtle
+
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__(visible=False)
+        self.penup()
+        self.goto(-250, 250)
+
+    def write_level(self, text: str):
+        self.clear()
+        self.write(text, font=FONT)
