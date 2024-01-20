15408b52276c435794efd3b3548d3d2c0fa319ab
OlekZima <zimenkoalex34@gmail.com>
2024-01-21 00:37:12 +0100

    Created car class with initial constructor

---
+++ Intermediate(15-31)/Day_22/car.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+import random as rn
+
+
+class Car(Turtle):
+    def __init__(self):
+        super().__init__(shape="square")
+        self.penup()
+        self.speed("fastest")
+        self.goto(1000, 0)
+        self.setheading(180)
+        self.shapesize(1, 2)
