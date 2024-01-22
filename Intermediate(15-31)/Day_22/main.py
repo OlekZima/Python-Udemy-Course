6722e97db3f212c737149c9bb09ee1176d5d763a
OlekZima <zimenkoalex34@gmail.com>
2024-01-22 19:14:02 +0100

    Refactor game logic and improve gameplay

--- Intermediate(15-31)/Day_22/main.py
+++ Intermediate(15-31)/Day_22/main.py
@@ -1,31 +1,46 @@
-import time
+from random import randint
+from time import sleep
 from turtle import Screen
-from player import FINISH_LINE_Y, Player
+from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-from car import Car
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.listen()
 
-player = Player()
-scoreboard = Scoreboard()
-car_manager = CarManager()
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.title("Cross the Road")
+    screen.tracer(0)
+    screen.listen()
 
-car_manager.create_car()
+    player = Player()
+    scoreboard = Scoreboard()
+    car_manager = CarManager()
 
-game_is_on = True
-while game_is_on:
-    screen.onkeypress(key="w", fun=player.move_up)
-    if player.ycor() >= FINISH_LINE_Y:
-        player.reset_pos()
-    car_manager.move_cars()
+    level = 1
+    game_speed_multipier = 0.7
+    game_speed: float = 0.1
 
-    game_is_on = not player.is_car_collision(car_manager.get_cars())
+    game_is_on = True
+    while game_is_on:
+        screen.onkeypress(key="w", fun=player.move_up)
+        scoreboard.write_level(f"Level {level}")
+        if player.ycor() >= player.FINISH_LINE_Y:
+            player.reset_pos()
+            level += 1
+            game_speed *= game_speed_multipier
 
-    time.sleep(0.1)
-    screen.update()
+        car_manager.move_cars(speed_multiplier=0)
+        if randint(1, 7) == 2:
+            car_manager.create_car(randint(-250, 250))
+        car_manager.retrieve_cars()
 
-print("GG")
+        game_is_on = not player.is_car_collision(car_manager.get_cars())
+        sleep(game_speed)
+        screen.update()
+
+    print("Game over!")
+
+
+if __name__ == "__main__":
+    main()
