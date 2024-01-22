3d73d71557fd9e1faa9ed10e358c5bcee60d3579
OlekZima <zimenkoalex34@gmail.com>
2024-01-22 19:13:53 +0100

    Refactor player class in player.py

--- Intermediate(15-31)/Day_22/player.py
+++ Intermediate(15-31)/Day_22/player.py
@@ -1,25 +1,24 @@
-from turtle import Turtle, distance
+from turtle import Turtle
 from typing import List
 from car import Car
-
-STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
     def __init__(self):
         super().__init__(shape="turtle")
+        self.FINISH_LINE_Y = 280
+        self.MOVE_DISTANCE = 10
         self.speed("fastest")
         self.penup()
         self.setheading(90)
-        self.goto(STARTING_POSITION)
+        self.STARTING_POSITION = (0, -280)
+        self.goto(self.STARTING_POSITION)
 
     def move_up(self):
-        self.forward(MOVE_DISTANCE)
+        self.forward(self.MOVE_DISTANCE)
 
     def reset_pos(self):
-        self.goto(STARTING_POSITION)
+        self.goto(self.STARTING_POSITION)
 
     def is_car_collision(self, cars: List[Car]) -> bool:
         for car in cars:
