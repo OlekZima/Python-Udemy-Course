362ed860a788875a02ac825160b6e119c829066c
OlekZima <zimenkoalex34@gmail.com>
2024-01-22 19:22:56 +0100

    Add level tracking and level increase functionality to Scoreboard class

--- Intermediate(15-31)/Day_22/scoreboard.py
+++ Intermediate(15-31)/Day_22/scoreboard.py
@@ -5,6 +5,7 @@ from typing import Tuple
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__(visible=False)
+        self.level: int = 1
         self.penup()
         self.goto(-250, 250)
         self.FONT: Tuple[str, int, str] = ("Courier", 24, "normal")
@@ -12,3 +13,10 @@ class Scoreboard(Turtle):
     def write_level(self, text: str):
         self.clear()
         self.write(text, font=self.FONT)
+
+    def increase_level_update_scoreboard(self):
+        self.level += 1
+        self.write_level(f"Level {self.level}")
+
+    def get_level(self) -> int:
+        return self.level
