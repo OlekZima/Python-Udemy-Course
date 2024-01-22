334b0c2bc282b8fa76232f5205e2691656f2bd65
OlekZima <zimenkoalex34@gmail.com>
2024-01-22 19:23:02 +0100

    Refactor game speed and level logic

--- Intermediate(15-31)/Day_22/main.py
+++ Intermediate(15-31)/Day_22/main.py
@@ -16,27 +16,33 @@ def main():
     player = Player()
     scoreboard = Scoreboard()
     car_manager = CarManager()
-
-    level = 1
+    """
+    Came with idea for 2 possible implementations for speed/difficulty increasing.
+    First one:
+        Some multiplier that decreases wait time in sleep fun (end of the file)
+    
+    Second one:
+        Just increase delta_x for moving cars, but at the highest Levels cars would just teleport like
+        half a screen
+    """
     game_speed_multipier = 0.7
     game_speed: float = 0.1
 
     game_is_on = True
     while game_is_on:
         screen.onkeypress(key="w", fun=player.move_up)
-        scoreboard.write_level(f"Level {level}")
         if player.ycor() >= player.FINISH_LINE_Y:
             player.reset_pos()
-            level += 1
+            scoreboard.increase_level_update_scoreboard()
             game_speed *= game_speed_multipier
 
-        car_manager.move_cars(speed_multiplier=0)
+        car_manager.move_cars(speed_multiplier=0)  # for the second option must be the level from `scoreboard.get_level()`
         if randint(1, 7) == 2:
             car_manager.create_car(randint(-250, 250))
         car_manager.retrieve_cars()
 
         game_is_on = not player.is_car_collision(car_manager.get_cars())
-        sleep(game_speed)
+        sleep(game_speed)  # for the second option must be const 0.1
         screen.update()
 
     print("Game over!")
