5586ef5adc9749c3b56512faa8b650822beaffbc
OlekZima <zimenkoalex34@gmail.com>
2024-01-25 17:16:00 +0100

    File operations modes

--- Intermediate(15-31)/Day_24/files_lesson/main.py
+++ Intermediate(15-31)/Day_24/files_lesson/main.py
@@ -1,3 +1,10 @@
-with open("my_file.txt") as file:
+with open(file="my_file.txt") as file:
     file_data: str = file.read()
     print(file_data)
+
+
+with open("my_file.txt", mode="a") as file:
+    file.write("New text.\n")
+
+with open("new_new_file.txt", mode="a") as file_new:
+    file_new.write("Hi!")
