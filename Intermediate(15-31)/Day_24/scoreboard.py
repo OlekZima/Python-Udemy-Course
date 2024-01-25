451ba5d0edbeed3a63683c637ff25782f2d2cfcc
OlekZima <zimenkoalex34@gmail.com>
2024-01-25 16:56:21 +0100

    Refactor Scoreboard class to include high score tracking

--- Intermediate(15-31)/Day_24/scoreboard.py
+++ Intermediate(15-31)/Day_24/scoreboard.py
@@ -6,17 +6,25 @@ class Scoreboard(Turtle):
     def __init__(self):
         self.FONT: Tuple[str, int, str] = ("Arial", 24, "bold")
         self.ALIGN: str = "center"
-
         super().__init__()
         self.hideturtle()
         self.penup()
         self.goto(x=0, y=250)
         self.score = 0
 
+        self.high_score = 0
         self._write_score()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+        self.score = 0
+
+        self._write_prompt(f"Score: {self.score} High Score: {self.high_score}")
+
     def _write_score(self):
-        self._write_prompt(f"Score: {self.score}")
+        self._write_prompt(f"Score: {self.score} High Score: {self.high_score}")
 
     def _write_prompt(self, prompt: str, is_clear: bool = True):
         if is_clear:
@@ -31,8 +39,7 @@ class Scoreboard(Turtle):
 
     def increment(self):
         self.score += 1
-        self._write_score()
+        if self.score > self.high_score:
+            self.high_score = self.score
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self._write_prompt(f"Game over!", is_clear=False)
+        self._write_prompt(f"Score: {self.score} High Score: {self.high_score}")
