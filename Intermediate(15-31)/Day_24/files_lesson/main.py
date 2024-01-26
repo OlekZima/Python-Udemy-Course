b39f669ea503abba360c43593c548d96df4809ca
OlekZima <zimenkoalex34@gmail.com>
2024-01-26 19:58:11 +0100

    Absolute and relative paths

--- Intermediate(15-31)/Day_24/files_lesson/main.py
+++ Intermediate(15-31)/Day_24/files_lesson/main.py
@@ -1,10 +1,10 @@
-with open(file="my_file.txt") as file:
+with open(file="C:/Users/Olek Zima/Desktop/my_file.txt") as file:
     file_data: str = file.read()
     print(file_data)
 
 
-with open("my_file.txt", mode="a") as file:
-    file.write("New text.\n")
+# with open("my_file.txt", mode="a") as file:
+#     file.write("New text.\n")
 
-with open("new_new_file.txt", mode="a") as file_new:
-    file_new.write("Hi!")
+# with open("new_new_file.txt", mode="a") as file_new:
+#     file_new.write("Hi!")
