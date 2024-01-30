728e289edba8a4163ae31d262c883a24865cb3d9
OlekZima <zimenkoalex34@gmail.com>
2024-01-30 19:07:06 +0100

    NATO alphabet project is done

--- Intermediate(15-31)/Day_26/nato_alphabet/main.py
+++ Intermediate(15-31)/Day_26/nato_alphabet/main.py
@@ -2,3 +2,25 @@ from typing import Dict, List
 import pandas as pd
 
 
+def main():
+    alphabet_df: pd.DataFrame = pd.DataFrame()
+    try:
+        alphabet_df: pd.DataFrame = pd.read_csv("./nato_phonetic_alphabet.csv")
+    except FileExistsError:
+        print("File doesn't exist.")
+    except:
+        print("Smth is broken.")
+
+    alphabet_dict: Dict[str, str] = {
+        row["letter"]: row["code"] for (_, row) in alphabet_df.iterrows()
+    }
+
+    user_input = input("Enter a word: ").upper()
+
+    result = [alphabet_dict.get(letter) for letter in user_input]
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
