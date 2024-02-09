00726ff15fd757db99b73b3767bda310a04367c2
OlekZima <zimenkoalex34@gmail.com>
2024-02-09 22:11:58 +0100

    Add functions for addition and calculation

--- Intermediate(15-31)/Day_27/playground.py
+++ Intermediate(15-31)/Day_27/playground.py
@@ -1,12 +1,22 @@
-def add(*nums: int) -> int:
+def add(*nums: int) -> int:  # *args is a tuple
     overall_sum = 0
     for n in nums:
         overall_sum += n
     return overall_sum
 
 
+def calculate(n, **kwargs):  # **kwargs is a dict
+    print(kwargs)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
 def main():
     print(add(1, 2, 3, 4, 5, 6, 7, 6, 11, 23, 1, 23, 123, 1, 23, 145, 15, 1, 25))
+
+    calculate(2, add=3, multiply=5)
 
 
 if __name__ == "__main__":
