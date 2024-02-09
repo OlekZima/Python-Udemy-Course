c83a22b2c18d8c64059db0bdb67648a54a3cfb10
OlekZima <zimenkoalex34@gmail.com>
2024-02-09 22:22:14 +0100

    Add Car class and update calculate function

--- Intermediate(15-31)/Day_27/playground.py
+++ Intermediate(15-31)/Day_27/playground.py
@@ -1,3 +1,12 @@
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
 def add(*nums: int) -> int:  # *args is a tuple
     overall_sum = 0
     for n in nums:
@@ -5,18 +14,21 @@ def add(*nums: int) -> int:  # *args is a tuple
     return overall_sum
 
 
-def calculate(n, **kwargs):  # **kwargs is a dict
+def calculate(n, **kwargs) -> int | float:  # **kwargs is a dict
     print(kwargs)
 
     n += kwargs["add"]
     n *= kwargs["multiply"]
-    print(n)
+    return n
 
 
 def main():
     print(add(1, 2, 3, 4, 5, 6, 7, 6, 11, 23, 1, 23, 123, 1, 23, 145, 15, 1, 25))
 
-    calculate(2, add=3, multiply=5)
+    print(calculate(2, add=3, multiply=5))
+
+    my_car = Car(make="toyota", model="hacharoku", seats=2, color="white")
+    print(my_car.model)
 
 
 if __name__ == "__main__":
