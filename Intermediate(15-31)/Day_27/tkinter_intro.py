16b6c046ca91acd58aa8ab47d1795506901a5886
OlekZima <zimenkoalex34@gmail.com>
2024-02-09 21:57:00 +0100

    Tkinter intro and some stuff about args default value

---
+++ Intermediate(15-31)/Day_27/tkinter_intro.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("My TKinter program")
+window.minsize(width=500, height=300)
+
+my_label = tk.Label(text="Hi I'm a label!", font=("Arial", 24, "italic"))
+my_label.pack(side="right", expand=False)
+
+
+import turtle as t
+
+tim = t.Turtle()
+tim.write("text")
+
+
+tk.mainloop()
