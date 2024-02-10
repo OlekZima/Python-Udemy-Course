4881115683710c2774c5a6410da79fe1c129066c
OlekZima <zimenkoalex34@gmail.com>
2024-02-10 19:30:44 +0100

    Add label, button, and entry widgets to the tkinter program

--- Intermediate(15-31)/Day_27/tkinter_intro.py
+++ Intermediate(15-31)/Day_27/tkinter_intro.py
@@ -4,9 +4,31 @@ window = tk.Tk()
 window.title("My TKinter program")
 window.minsize(width=500, height=300)
 
-my_label = tk.Label(text="Hi I'm a label!", font=("Arial", 24, "italic"))
-my_label.pack(side="right", expand=False)
+# Label
 
+my_label = tk.Label(text="Hi I'm a label!", font=("Arial", 24, "italic"))
+my_label.pack()
+
+my_label["text"] = "New text"
+my_label.config(text="Newer text")
+
+# Button
+counter = 0
+
+
+def button_clicked():
+    global counter
+    counter += 1
+    my_label.config(text=f"Clicked {counter} times")
+
+
+button = tk.Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input = tk.Entry()
+input.pack()
 
 
 tk.mainloop()
