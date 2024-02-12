81a52fa0c1684791a79042808cac16fb462df56e
Olek Zima <zimenkoalex34@gmail.com>
2024-02-12 21:17:32 +0100

    Some tests with placing widgets on the window

--- Intermediate(15-31)/Day_27/tkinter_intro.py
+++ Intermediate(15-31)/Day_27/tkinter_intro.py
@@ -18,23 +18,23 @@ window.minsize(width=500, height=300)
 my_label = tk.Label(text="Hi I'm a label!", font=("Arial", 24, "italic"))
 my_label["text"] = "New text"
 my_label.config(text="Newer text")
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 
 # Button
 
 button = tk.Button(text="Click me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
 # Entry
 
 input = tk.Entry(width=10)
-input.pack()
+input.grid(column=3, row=2)
 
 button_entry = tk.Button(
     text="Click me too!", command=lambda: my_label.config(text=input.get())
 )
-button_entry.pack()
+button_entry.grid(column=2, row=0)
 
 
 tk.mainloop()
--- Intermediate(15-31)/Day_27/tkinter_widget_demo.py
+++ Intermediate(15-31)/Day_27/tkinter_widget_demo.py
@@ -86,9 +86,9 @@ def radio_used():
 # Variable to hold on to which radio button value is checked.
 radio_state = IntVar()
 radiobutton1 = Radiobutton(text="Option1", value=1,
-                           variable=radio_state, command=radio_used)
+                            variable=radio_state, command=radio_used)
 radiobutton2 = Radiobutton(text="Option2", value=2,
-                           variable=radio_state, command=radio_used)
+                            variable=radio_state, command=radio_used)
 radiobutton1.pack()
 radiobutton2.pack()
 
