73231fda3987f7a2fa39c484a5f09dee54a5d623
Olek Zima <zimenkoalex34@gmail.com>
2024-02-13 12:49:51 +0100

    Design is done

--- Intermediate(15-31)/Day_28/main.py
+++ Intermediate(15-31)/Day_28/main.py
@@ -1,4 +1,4 @@
-
+import tkinter as tk
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,10 +9,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+tomato_img = tk.PhotoImage(file="./tomato.png")
+canvas = tk.Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 111, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", font=(
+    FONT_NAME, 28, "bold"), fill="white")
+canvas.pack()
+
+window.mainloop()
