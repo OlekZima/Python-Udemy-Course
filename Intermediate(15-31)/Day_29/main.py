e4b974510ef7b618726d3f18eaba4294ebebcece
Olek Zima <zimenkoalex34@gmail.com>
2024-02-15 16:45:57 +0100

    Add save_data function and update UI for password manager

--- Intermediate(15-31)/Day_29/main.py
+++ Intermediate(15-31)/Day_29/main.py
@@ -3,6 +3,17 @@ import tkinter as tk
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save_data():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    data = f"{website} | {email} | {password}"
+
+    with open("./data.txt", "a") as f:
+        f.write(data)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = tk.Tk()
@@ -21,6 +32,7 @@ website_label = tk.Label(text="Website:", bg="white")
 website_label.grid(row=1, column=0)
 
 website_entry = tk.Entry(width=35)
+website_entry.focus()
 website_entry.grid(row=1, column=1, columnspan=2)
 
 # Email/Username section
@@ -28,6 +40,7 @@ email_label = tk.Label(text="Email/Username:", bg="white")
 email_label.grid(row=2, column=0)
 
 email_entry = tk.Entry(width=35)
+email_entry.insert(tk.END, "name@email.com")
 email_entry.grid(row=2, column=1, columnspan=2)
 
 # Password section
@@ -40,7 +53,7 @@ password_entry.grid(row=3, column=1, sticky="E")
 generate_pass_btn = tk.Button(text="Generate Password")
 generate_pass_btn.grid(row=3, column=2)
 
-add_btn = tk.Button(text="Add", width=36)
+add_btn = tk.Button(text="Add", width=36, command=save_data)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
