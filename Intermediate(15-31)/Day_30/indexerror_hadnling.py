e26b0e100b012cde79aebb70cab7b0ec82adeeee
OlekZima <zimenkoalex34@gmail.com>
2024-02-16 20:08:36 +0100

    Fix IndexError handling in make_pie function

--- Intermediate(15-31)/Day_30/indexerror_hadnling.py
+++ Intermediate(15-31)/Day_30/indexerror_hadnling.py
@@ -4,9 +4,10 @@ fruits = ["Apple", "Pear", "Orange"]  # eval(input())
 def make_pie(index: int):
     try:
         fruit = fruits[index]
-        print(fruit + " pie")
     except IndexError:
         print("Fruit pie")
+    else:
+        print(fruit + " pie")
 
 
 make_pie(4)
