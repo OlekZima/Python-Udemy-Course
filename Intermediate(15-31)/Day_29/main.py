4960cf39612530b6f4c4942c5ce08a6663b7fa9b
OlekZima <zimenkoalex34@gmail.com>
2024-02-16 16:30:37 +0100

    Add whole Day_5 pasword generator

--- Intermediate(15-31)/Day_29/main.py
+++ Intermediate(15-31)/Day_29/main.py
@@ -1,8 +1,33 @@
 import tkinter as tk
 from tkinter import messagebox
-
+from random import randint, choice
+from string import ascii_letters
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+print("Welcome to the PyPassword Generator!")
+
+pass_len = int(input("How many letters would you like in your password?\n"))
+symbols_num = int(input("How many symbols would you like?\n"))
+numbers_num = int(input("How many numbers would you like?\n"))
+
+password = ""
+
+for i in range(pass_len):
+    password += choice(ascii_letters)
+
+symbols = ['!', '#', '$', '%', '&', '()', ')', '*', '+']
+
+for i in range(symbols_num):
+    password += choice(symbols)
+
+for i in range(numbers_num):
+    password += str(randint(0, 9))
+
+# password = "".join(sample(password, len(password)))
+password = "".join(list(password))
+
+print(f"Your password is {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
