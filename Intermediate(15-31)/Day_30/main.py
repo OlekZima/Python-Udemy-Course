859c134393edc6acdb38d422f191ab3924cf9174
OlekZima <zimenkoalex34@gmail.com>
2024-02-16 19:56:04 +0100

    Raise keyword test

--- Intermediate(15-31)/Day_30/main.py
+++ Intermediate(15-31)/Day_30/main.py
@@ -2,24 +2,29 @@
 # with open("a_file.txt") as file:
 #     file.read()
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    value = a_dictionary["key"]
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Smth")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was close.")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     value = a_dictionary["key"]
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Smth")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise KeyError("WTF! Raised!")
 
+height = float(input("Height [m]: "))
+weight = int(input("Weight [kg]: "))
 
+if height > 3:
+    raise ValueError("Height should not be over 3 meters.")
 
+bmi = weight / height**2
+print(bmi)
 
 # KeyError
 # a_dictionary = {"key": "value"}
