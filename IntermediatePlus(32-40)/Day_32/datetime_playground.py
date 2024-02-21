865fd4008b05210c61e129bfa1ec391a76f60600
OlekZima <zimenkoalex34@gmail.com>
2024-02-21 14:19:08 +0100

    Add datetime playground script

---
+++ IntermediatePlus(32-40)/Day_32/datetime_playground.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(2002, 9, 18, 12)
+print(date_of_birth)
