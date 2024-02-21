c7bf6839002c4a66b60241d65618c6255805ce55
OlekZima <zimenkoalex34@gmail.com>
2024-02-21 13:53:44 +0100

    Refactor email sending code

--- IntermediatePlus(32-40)/Day_32/main.py
+++ IntermediatePlus(32-40)/Day_32/main.py
@@ -4,4 +4,8 @@ from credentials import my_email, app_password, mail_send
 with smtp.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
     connection.login(user=my_email, password=app_password)  # type: ignore
-    connection.sendmail(from_addr=my_email, to_addrs=mail_send, msg="Subject:Greetings\n\nMy email for myself via smtp and python.")  # type: ignore
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=mail_send,
+        msg="Subject:Greetings\n\nMy email for myself via smtp and python.",
+    )  # type: ignore
