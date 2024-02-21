a1873bedf23fb539829f10cf8cf3938474258715
OlekZima <zimenkoalex34@gmail.com>
2024-02-21 13:54:58 +0100

    Fix login credentials in main.py

--- IntermediatePlus(32-40)/Day_32/main.py
+++ IntermediatePlus(32-40)/Day_32/main.py
@@ -3,9 +3,9 @@ from credentials import my_email, app_password, mail_send
 
 with smtp.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
-    connection.login(user=my_email, password=app_password)  # type: ignore
+    connection.login(user=my_email, password=app_password)
     connection.sendmail(
         from_addr=my_email,
         to_addrs=mail_send,
         msg="Subject:Greetings\n\nMy email for myself via smtp and python.",
-    )  # type: ignore
+    )
