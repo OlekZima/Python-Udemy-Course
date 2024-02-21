5f4fe5b8131538f3b5870053f65213c8b5152176
OlekZima <zimenkoalex34@gmail.com>
2024-02-21 14:23:21 +0100

    Refactor

--- IntermediatePlus(32-40)/Day_32/quote_sender/quote_sender.py
+++ IntermediatePlus(32-40)/Day_32/quote_sender/quote_sender.py
@@ -1,13 +1,13 @@
 import datetime as dt
 from random import choice
 import smtplib as smtp
+import credentials
 
 
 def get_random_line_from_file(path: str) -> str:
     try:
         with open(path, "r") as f:
-            content = f.read().split("\n")
-            line = choice(content)
+            line = choice(f.read().split("\n"))
     except FileNotFoundError:
         print("File does not exist!")
         exit()
