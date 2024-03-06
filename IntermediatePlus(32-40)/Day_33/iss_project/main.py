e974e7483a7fc6ec287c06b336f03cda12c5affa
OlekZima <zimenkoalex34@gmail.com>
2024-03-06 20:30:38 +0100

    Add functions to get ISS position and check if it is above a given location

--- IntermediatePlus(32-40)/Day_33/iss_project/main.py
+++ IntermediatePlus(32-40)/Day_33/iss_project/main.py
@@ -1,5 +1,27 @@
+from typing import Tuple
 import requests
 from datetime import datetime
+
+
+def get_iss_position() -> Tuple[float, float]:
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+    response.raise_for_status()
+    data = response.json()
+    longitude = data["iss_position"]["longitude"]
+    latitude = data["iss_position"]["latitude"]
+
+    return (float(latitude), float(longitude))
+
+
+def is_iss_above(lat: float, lng: float, iss_position: Tuple[float, float]) -> bool:
+    if iss_position[0] - 5 < lat < iss_position[0] + 5:
+        is_lat = True
+    if iss_position[1] - 5 < lng < iss_position[1] + 5:
+        is_lng = True
+
+    return is_lat and is_lng
+
 
 base_url = "https://api.sunrise-sunset.org/json"
 
@@ -16,8 +38,8 @@ data = response.json()
 sunrise_time_unix: str = data["results"]["sunrise"]
 sunset_time_unix: str = data["results"]["sunset"]
 
-sunrise_hour = sunrise_time_unix.split("T")[1].split(":")[0]
-sunset_hour = sunset_time_unix.split("T")[1].split(":")[0]
+sunrise_hour = int(sunrise_time_unix.split("T")[1].split(":")[0])
+sunset_hour = int(sunset_time_unix.split("T")[1].split(":")[0])
 
 time_now_hour = datetime.now().time().hour
 print(sunrise_hour, sunset_hour)
